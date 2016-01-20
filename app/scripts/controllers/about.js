18507b48422a93e3aca6901a36a7f7a186758ec5
Adan Ferguson <adanferguson@gmail.com>
2016-01-20 15:37:30 -0700

    Completely changed everything to a yeoman generated app
    LOL

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name adanFergusonWebsiteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the adanFergusonWebsiteApp
+ */
+angular.module('adanFergusonWebsiteApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name adanFergusonWebsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the adanFergusonWebsiteApp
+ */
+angular.module('adanFergusonWebsiteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
