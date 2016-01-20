5f70317d1b1f6b6e65be13fb286ab1805be3e261
Adan Ferguson <adanferguson@gmail.com>
2016-01-19 23:27:25 -0700

    Getting server running

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.set('views', process.cwd() + '/src');
+
+app.use(express.static('src'));
+
+app.get('/', function (req, res) {
+   res.render('index', {page: 'home'});
+});
+
+app.get('/:id', function (req, res) {
+   res.render('index', {page: req.param('id')});
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+});
