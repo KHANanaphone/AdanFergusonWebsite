4be709b6477ee225bfd70e8d8101b7e89b098f51
Adan Ferguson <adanferguson@gmail.com>
2016-01-25 19:08:14 -0700

    Complete refactoring to more simpler stuff

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+router.get('/*', function(req, res) {
+
+    var url = req.url.substring(1);
+
+    if(!url)
+        url = '';
+
+    var pages = {
+        '': {name: 'Home', view: 'home'},
+        about: {name: 'About Me', view: 'about'},
+        links: {name: 'Links', view: 'links'},
+        contact: {name: 'Contact', view: 'contact'},
+    };
+
+    var currentPage = pages[url];
+
+    console.log('CP: ' + JSON.stringify(currentPage));
+
+    if(!currentPage)
+        return res.redirect('/');
+
+    res.render(currentPage.view, {
+        title: 'Express',
+        pages: pages,
+        currentPage: currentPage.view
+    });
+});
+
+module.exports = router;
